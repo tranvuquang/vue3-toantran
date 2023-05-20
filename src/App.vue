9be6325a3aa2d0688e39a363f81503fbe705d555
quangminh <quangminh@gmail.com>
2023-05-20 15:14:09 +0700

    br01_basic01

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,37 @@
 export default {
   name: "App",
   components: {},
+  data() {
+    return {
+      teamName: "RHP Team",
+      firstName: "Toan",
+      lastName: "Tran",
+      isHello: false,
+      isShowModal: false,
+    };
+  },
+  methods: {
+    onChangeName() {
+      this.firstName = "Sky";
+      this.lastName = "Albert";
+      this.isShowModal = !this.isShowModal;
+    },
+  },
 };
 </script>
 
 <template>
-  <div id="app">App</div>
+  <div id="app">
+    <h1>
+      Hello from {{ teamName }} and
+      {{ !isHello ? `${firstName} ${lastName}` : "" }}
+    </h1>
+    <button @click="onChangeName">Change Name</button>
+
+    <div class="modal1" v-if="isShowModal">
+      <h2>Modal content</h2>
+    </div>
+  </div>
 </template>
 
 <style scoped>
