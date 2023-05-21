944c3509a8171448211ea1d70a04db07d01ba65e
quangminh <quangminh@gmail.com>
2023-05-21 07:11:25 +0700

    br06_bind_input

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script>
-import Modal from "./components/Modal.vue";
+import QuizForm from "./components/QuizForm.vue";
 export default {
   name: "App",
-  components: { Modal },
+  components: { QuizForm },
   data() {
     return {
       isShowModal: false,
@@ -19,26 +19,7 @@ export default {
 <template>
   <div id="app">
     <h1>this is app</h1>
-    <button @click="toggleModal">Click me</button>
-    <Teleport to="body">
-      <Modal
-        title="abc"
-        content="def"
-        theme="123"
-        v-if="isShowModal"
-        v-on:closeModal="toggleModal"
-      >
-        <template v-slot:header>
-          <h2>This is header slot</h2>
-        </template>
-        <label for="">Name</label>
-        <input type="text" />
-
-        <template v-slot:footer>
-          <h2>This is footer slot</h2>
-        </template>
-      </Modal>
-    </Teleport>
+    <QuizForm />
   </div>
 </template>
 
@@ -48,4 +29,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
+
 </style>
